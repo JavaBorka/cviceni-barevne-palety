2e1855fb110307b10f495e8c76654c9d270aba70
JavaBorka <borka34@gmail.com>
2023-10-01 06:47:48 +0200

    Upravená komponenta Palette

--- src/components/Palette/index.jsx
+++ src/components/Palette/index.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import classNames from "classnames";
+import SchemeColor from "../SchemeColor";
+import './style.css'
 
-const palettes = [
-  {
-    name: 'Mimosa Retreat',
-    image: '/img/mimosa-retreat.jpg',
-    attribution: {
-      name: 'Sara Cervera',
-      url: 'https://unsplash.com/photos/XkiONXX7i4o',
-    },
-    colors: ['#583e26', '#a78b71', '#f7c815', '#ec9704', '#9c4a1a'],
-    direction: 'vertical',
-    description: 'Brown, combined with shades of yellow, is a rather unusual combination that can be called really sweet. It is both citrusy, caramel, and has a slight hint of cinnamon, which gives it a special versatility. In summer it will invigorate, make you act and move, and in winter it will warm and relax.',
-  },
-];
-
-const Palette = (paletteData) => (
+const Palette = ({paletteData}) => (
     <div className="palette">
         <div className={classNames("palette-scheme", {"palette-scheme--vertical" : (paletteData.direction === 'vertical'), "palette-scheme--horizontal" : (paletteData.direction === 'horizontal')})}>
             <img className="scheme-image" src={paletteData.image} alt={paletteData.name} />
@@ -31,7 +19,7 @@ const Palette = (paletteData) => (
             <h2>{paletteData.name}</h2>
             <p>{paletteData.description}</p>
 
-            <p>Photo by <a href={paletteData.attribution.url} target="_blank">{paletteData.attribution.name}</a>.</p>
+            <p>Photo by <a href={paletteData.attribution.url} target="_blank">{paletteData.attribution.name}</a></p>
         </div>
     </div>
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,55 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import SchemeColor from './components/SchemeColor';
+import Palette from './components/Palette';
 import './style.css';
-
-// const palettes = [
-//   {
-//     name: 'Mimosa Retreat',
-//     image: '/img/mimosa-retreat.jpg',
-//     attribution: {
-//       name: 'Sara Cervera',
-//       url: 'https://unsplash.com/photos/XkiONXX7i4o',
-//     },
-//     colors: ['#583e26', '#a78b71', '#f7c815', '#ec9704', '#9c4a1a'],
-//     direction: 'vertical',
-//     description: 'Brown, combined with shades of yellow, is a rather unusual combination that can be called really sweet. It is both citrusy, caramel, and has a slight hint of cinnamon, which gives it a special versatility. In summer it will invigorate, make you act and move, and in winter it will warm and relax.',
-//   },
-//   {
-//     name: 'Ocean Waves',
-//     image: '/img/ocean-waves.jpg',
-//     attribution: {
-//       name: 'Christoffer Engström',
-//       url: 'https://unsplash.com/photos/wc9avd2RaN0',
-//     },
-//     colors: ['#012e4a', '#036280', '#378ba4', '#81bece', '#e8ede7'],
-//     direction: 'horizontal',
-//     description: 'Wildlife has given humanity an indescribable variety of color shades that, at first glance, cannot be combined for balance. However, the combination of dark greens canonically turns into a bright light green shade, growing into banana yellow. From the yellow tones, a sudden rich pink breaks through.',
-//   },
-//   {
-//     name: 'Santorini',
-//     image: '/img/santorini.jpg',
-//     attribution: {
-//       name: 'Jonathan Gallegos',
-//       url: 'https://unsplash.com/photos/L2ZM1TbOO-8',
-//     },
-//     colors: ['#16354d', '#6b99c3', '#d2d2d4', '#e4e5ea', '#0c151c'],
-//     direction: 'vertical',
-//     description: 'Do not know how to combine rigor, tenderness, perseverance and determination in one male image? Then use this palette. Dark blue and black will give the image confidence, denim blue – romanticism and mystery, and gray tones – lightness and freedom. No one can resist such a decision.',
-//   },
-//   {
-//     name: 'Night Sky',
-//     image: '/img/night-sky.jpg',
-//     attribution: {
-//       name: 'kazuend',
-//       url: 'https://unsplash.com/photos/2KXEb_8G5vo',
-//     },
-//     colors: ['#8689ac', '#587099', '#3f5576', '#2f3148', '#101116'],
-//     direction: 'vertical',
-//     description: 'A nightly and mysterious combination of shades of blue, which includes pale blue, green-blue, midnight blue, personifies the word “mystery”. But this riddle is permeated with a cold that penetrates deep into your mind, preventing you from getting too close to understanding what this palette is fraught with and why it sinks into your head so much.',
-//   },
-// ];
-
 
 // Původní zadání - data natvrdo zadané do aplikace
 // const App = () => {
@@ -104,7 +56,6 @@ import './style.css';
 //   );
 // };
 
-
 const palettes = [
   {
     name: 'Mimosa Retreat',
@@ -117,7 +68,41 @@ const palettes = [
     direction: 'vertical',
     description: 'Brown, combined with shades of yellow, is a rather unusual combination that can be called really sweet. It is both citrusy, caramel, and has a slight hint of cinnamon, which gives it a special versatility. In summer it will invigorate, make you act and move, and in winter it will warm and relax.',
   },
+  {
+    name: 'Ocean Waves',
+    image: '/img/ocean-waves.jpg',
+    attribution: {
+      name: 'Christoffer Engström',
+      url: 'https://unsplash.com/photos/wc9avd2RaN0',
+    },
+    colors: ['#012e4a', '#036280', '#378ba4', '#81bece', '#e8ede7'],
+    direction: 'horizontal',
+    description: 'Wildlife has given humanity an indescribable variety of color shades that, at first glance, cannot be combined for balance. However, the combination of dark greens canonically turns into a bright light green shade, growing into banana yellow. From the yellow tones, a sudden rich pink breaks through.',
+  },
+  {
+    name: 'Santorini',
+    image: '/img/santorini.jpg',
+    attribution: {
+      name: 'Jonathan Gallegos',
+      url: 'https://unsplash.com/photos/L2ZM1TbOO-8',
+    },
+    colors: ['#16354d', '#6b99c3', '#d2d2d4', '#e4e5ea', '#0c151c'],
+    direction: 'vertical',
+    description: 'Do not know how to combine rigor, tenderness, perseverance and determination in one male image? Then use this palette. Dark blue and black will give the image confidence, denim blue – romanticism and mystery, and gray tones – lightness and freedom. No one can resist such a decision.',
+  },
+  {
+    name: 'Night Sky',
+    image: '/img/night-sky.jpg',
+    attribution: {
+      name: 'kazuend',
+      url: 'https://unsplash.com/photos/2KXEb_8G5vo',
+    },
+    colors: ['#8689ac', '#587099', '#3f5576', '#2f3148', '#101116'],
+    direction: 'vertical',
+    description: 'A nightly and mysterious combination of shades of blue, which includes pale blue, green-blue, midnight blue, personifies the word “mystery”. But this riddle is permeated with a cold that penetrates deep into your mind, preventing you from getting too close to understanding what this palette is fraught with and why it sinks into your head so much.',
+  },
 ];
+
 
 const App = () => {
   return (
@@ -126,7 +111,9 @@ const App = () => {
         <h1>Barevné palety</h1>
       </header>
       <main>
-
+        {palettes.map((palette) => 
+        <Palette key={palette.name} paletteData={palette} />
+        )}
       </main>
       <footer>
         <p>Czechitas, Digitální akademie: Web</p>
